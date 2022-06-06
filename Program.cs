8f61078f5fd3b0d86eb155a883bb125a061345ad
“00Amit0” <amit836961@gmail.com>
2022-06-07 02:33:17 +0530

    Welcome to Stacks and Queues

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackAndQueue;
+
+namespace StacksandQueue
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue program using Linked List");
+        }
+    }
+}
